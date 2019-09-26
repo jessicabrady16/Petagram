051d0a5bbc1520d52b77a2e23642067808e9e77d
Jessica Brady <jessicabrady16@gmail.com>
2019-09-26 16:31:15 -0600

    added a new action on grams controller and updated a test for it

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :request do
-  describe "grams#index action" do
-    it "should successfully show the page" do
+  describe 'grams#index action' do
+    it 'should successfully show the page' do
       get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'grams#new action' do
+    it 'should successfully show the new form' do
+      get new_gram_path
       expect(response).to have_http_status(:success)
     end
   end
