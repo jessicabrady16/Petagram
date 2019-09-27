cd266aabfb1b94312cd4d9ca207f693dbf6a0a72
Jessica Brady <jessicabrady16@gmail.com>
2019-09-27 17:35:59 -0600

    added request file for gram requests and deleted content out of controller and helper files

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,36 +1,4 @@
-require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
- 
-  RSpec.describe GramsController, type: :request do
-    describe 'grams#index action' do
-      it 'should successfully show the page' do
-        get root_path
-        expect(response).to have_http_status(:success)
-      end
-    end
 
-    describe 'grams#new action' do
-      it 'should successfully show the new form' do
-        get new_gram_path
-        expect(response).to have_http_status(:success)
-      end
-    end
-
-    describe 'grams#create action' do
-      it 'should successfully create a new gram in our database' do
-        post grams_path, params: { gram: { message: 'Hello!' } }
-        expect(response).to redirect_to root_path
-
-        gram = Gram.last
-        expect(gram.message).to eq('Hello!')
-      end
-
-      it 'should properly deal with validation errors' do
-        post grams_path, params: { gram: { message: '' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(Gram.count).to eq 0
-      end
-    end
-  end
 end
